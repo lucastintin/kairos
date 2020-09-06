9f7a955736514e2fc0b8e98ef4db02e858efb832
Lucas Amiune <lucastintin@gmail.com>
2020-09-06 03:34:12 -0300

    Rotas de inserção de batidas

--- routes/api/batidas.js
+++ routes/api/batidas.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => res.send('Rota de Batidas'));
 //@route    POST api/batidas
 //@desc     Rota de Registro de Batidas - CREATE
 //@desc     Manual do Proprio Usuario com Justificativa 
-//@access   public
+//@access   private
 router.post('/', auth, async (req, res) => {
 
     let { dtHoraBatida, justificativa} = req.body;
@@ -40,7 +40,7 @@ router.post('/', auth, async (req, res) => {
     }
 });
 
-//@route    POST api/batidas
+//@route    POST api/batidas/automatica
 //@desc     Rota de Registro de Batidas - CREATE
 //@desc     Automatica do Proprio Usuario sem Justificativa 
 //@access   public
@@ -52,7 +52,7 @@ router.post('/automatica', async (req, res) => {
         let usuario = await Usuario.findOne({ email });
 
         if (!usuario) {
-            res.status(400).json({ errors: [{ msg: 'Usuário não encontradp' }] });
+            res.status(400).json({ errors: [{ msg: 'Usuário não encontrado' }] });
         }
 
         const isMatch = await bcrypt.compare(senha, usuario.senha);
@@ -74,10 +74,50 @@ router.post('/automatica', async (req, res) => {
 
         res.status(200).send('Batida registrada com sucesso.');
 
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Erro na rota bat002');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro na rota bat002');
+    }
+});
+
+//@route    POST api/batidas/abono
+//@desc     Rota de Registro de Batidas - CREATE
+//@desc     Manual de Abono do Usuario pelo RH
+//@access   private
+router.post('/abono', auth,  async (req, res) => {
+
+    try {
+        const gestorId = req.usuarioId;
+        let gestor = await Usuario.findById({ _id: gestorId })
+
+        if (gestor.nivel == 1)  {
+            return res.status(400).json({ errors: [{ msg: 'Usuário não tem permissão para abonar' }] });
         }
+
+        let { email, dtHoraBatida } = req.body;
+
+        let funcionario = await Usuario.findOne({ email });
+        if (!funcionario) {
+            res.status(400).json({ errors: [{ msg: 'Email não encontrado' }] });
+        }
+
+        dtHoraBatida = moment(new Date(dtHoraBatida)).utc().format();
+
+        let batida = new Batida({
+            usuarioID: funcionario.id,
+            dtHoraBatida,
+            justificativa: `Abono por ${gestor.email}`,
+            enderecoIP: req.ip,
+            userAgent: req.userAgent
+        })
+        await batida.save();
+
+        res.status(200).send('Batida registrada com sucesso.');
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro na rota bat003');
+    }
 });
 
 
